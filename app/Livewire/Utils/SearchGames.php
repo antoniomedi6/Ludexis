910755787b4c3c79e5ee7c4dfc7dd9a3072f6f46
Antonio Medina <antoniomedi6@gmail.com>
2026-03-21 12:55:02 +0100

    Mejoras en la búsqueda de SearchBar.

--- app/Livewire/Utils/SearchGames.php
+++ app/Livewire/Utils/SearchGames.php
@@ -16,38 +16,47 @@ class SearchGames extends Component
         $games = collect();
 
         if (strlen($this->search) >= 2) {
-            $igdbGames = IGDBGame::select(['id', 'name', 'first_release_date', 'total_rating', 'slug'])
-                ->with(['cover' => ['url']])
-                ->search($this->search)
-                ->where('game_type', 0)
-                ->limit(5)
+            $igdbGames = IGDBGame::where('name', 'like', '%' . $this->search . '%')
+                ->orderBy('total_rating', 'desc')
+                ->select(['id', 'name', 'first_release_date', 'total_rating', 'slug', 'category'])
+                ->with(['cover' => ['image_id', 'url']])
+                ->limit(10)
                 ->get();
 
-            $games = $igdbGames->map(function ($igdbGame) {
-                $coverUrl = null;
-                $url = data_get($igdbGame, 'cover.url');
+            $games = $igdbGames
+                ->filter(function ($igdbGame) {
+                    return in_array($igdbGame->category ?? 0, [0, 8, 9]);
+                })
+                ->take(5)
+                ->map(function ($igdbGame) {
+                    $coverUrl = null;
 
-                if ($url) {
-                    $coverUrl = str_replace('t_thumb', 't_cover_big', $url);
-                }
+                    if (isset($igdbGame->cover) && isset($igdbGame->cover['image_id'])) {
+                        $coverUrl = 'https://images.igdb.com/igdb/image/upload/t_cover_big/' . $igdbGame->cover['image_id'] . '.jpg';
+                    } elseif (isset($igdbGame->cover) && isset($igdbGame->cover['url'])) {
+                        $coverUrl = str_replace('t_thumb', 't_cover_big', $igdbGame->cover['url']);
+                        if (str_starts_with($coverUrl, '//')) {
+                            $coverUrl = 'https:' . $coverUrl;
+                        }
+                    }
 
-                $releaseDate = data_get($igdbGame, 'first_release_date');
+                    $releaseDate = $igdbGame->first_release_date ?? null;
 
-                $placeholder = new Game();
-                $placeholder->igdb_id = $igdbGame->id;
-                $placeholder->title = $igdbGame->name;
-                $placeholder->cover_url = $coverUrl;
-                $placeholder->rating = $igdbGame->total_rating ?? 0;
-                $placeholder->slug = $igdbGame->slug;
+                    $placeholder = new Game();
+                    $placeholder->igdb_id = $igdbGame->id;
+                    $placeholder->title = $igdbGame->name;
+                    $placeholder->cover_url = $coverUrl;
+                    $placeholder->rating = $igdbGame->total_rating ?? 0;
+                    $placeholder->slug = $igdbGame->slug;
 
-                if ($releaseDate) {
-                    $placeholder->first_release_date = is_numeric($releaseDate)
-                        ? Carbon::createFromTimestamp($releaseDate)
-                        : Carbon::parse($releaseDate);
-                }
+                    if ($releaseDate) {
+                        $placeholder->first_release_date = is_numeric($releaseDate)
+                            ? Carbon::createFromTimestamp($releaseDate)
+                            : Carbon::parse($releaseDate);
+                    }
 
-                return $placeholder;
-            });
+                    return $placeholder;
+                })->values(); // Reindexar la colección
         }
 
         return view('livewire.utils.search-games', compact('games'));
